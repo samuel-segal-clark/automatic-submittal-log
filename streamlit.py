b81b0279ca38a4db172e6e1822a2ae8d532a2d12
Samuel Segal <samuel.b.segal@gmail.com>
2024-02-29 18:38:53 +0100

    Implement streamlit settings

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import pandas as pd
 
 st.write('# Automatic Submittal Log')
 
@@ -7,11 +6,46 @@ file_container = st.columns(2)
 procore_csv = file_container[0].file_uploader('Upload Procore CSV here',type=['csv'])
 submittal_log_xlsx = file_container[1].file_uploader('Upload Submittal Log XLSX here', type=['xlsx'])
 
-with st.expander('Settings'):
-    with st.form('settings_form'):
 
-        import_settings = st.file_uploader('Import settings', type=['ini'])
-        submitted = st.form_submit_button('Save Settings')
+#TODO Read this from a file (i.e. default settings)
+sub_log_settings = {
 
-        if submitted:
-            print('Submit!')
+}
+
+#TODO Read this from a file (i.e. settings types json)
+settings_input = {
+    'title-format': {
+        'name': 'Title Format',
+        'type': 'choice',
+        'choices': [
+            'Default',
+            'WMATA'
+        ]
+    },
+    'spec-section': {
+        'name': 'Spec Section Column',
+        'type': 'str'
+    }
+}
+
+settings_container = st.container()
+file_upload = settings_container.file_uploader('Import settings', type=['ini'])
+settings_expander = st.expander('Settings')
+
+form_elements = dict()
+with settings_expander.form('Input Settings'):
+    for obj_key in settings_input.keys():
+        set_obj = settings_input[obj_key]
+        new_element = None
+        match set_obj['type']:
+            case 'choice':
+                new_element = st.selectbox(
+                    label=set_obj['name'],
+                    options=set_obj['choices']
+                )
+            case 'str':
+                new_element = st.text_input(set_obj['name'])
+        form_elements[obj_key] = new_element
+    submit_button = st.form_submit_button('Submit')
+    if submit_button:
+        st.write('Settings submited! The settings have been downloaded to your computer to use in the future!')
