adbaa31211ff6f2b138b12901ccd2e6966f565ff
Samuel Segal <samuel.b.segal@gmail.com>
2024-03-14 17:18:29 +0100

    Init openpxyl log

---
+++ submittal-src/config.py
@@ -0,0 +1,4 @@
+class AutoLogConfig:
+
+    def __init__(self) -> None:
+        pass
---
+++ submittal-src/update_log.py
@@ -0,0 +1,8 @@
+from io import BytesIO
+
+from config import AutoLogConfig
+
+
+def update_log(xlsx_file: BytesIO, csv_file: BytesIO, config: AutoLogConfig) -> BytesIO:
+
+    return 'the new xlsx file'
